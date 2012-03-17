3c719bd53c0fc23a0e528e417de4054c982044fb
Ralf Saldana <ralfs16@gmail.com>
2012-03-17 16:59:38 -0500

    commit proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TDEW2012::Application.config.secret_token = '2d85e96d986a36e6e4a300350f04087c3f135c8988d289205f963d2e28e16e099c5513befb17147fe9ac37061b0fa0c49cc1677ab09f19176f91ac1b8294ef9e'
