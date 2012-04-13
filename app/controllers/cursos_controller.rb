9a9845e6ada0ace5a53ea56c81001acbeb358527
= <ralfs16@gmail.com>
2012-04-12 19:03:30 -0500

    mensajes validacion

--- app/controllers/cursos_controller.rb
+++ app/controllers/cursos_controller.rb
@@ -51,7 +51,7 @@ class CursosController < ApplicationController
 
     respond_to do |format|
       if @curso.save
-        format.html { redirect_to @curso, notice: 'Curso was successfully created.' }
+        format.html { redirect_to @curso, notice: 'Curso grabado' }
         format.json { render json: @curso, status: :created, location: @curso }
       else
         format.html { render action: "new" }
@@ -68,7 +68,7 @@ class CursosController < ApplicationController
 
     respond_to do |format|
       if @curso.update_attributes(params[:curso])
-        format.html { redirect_to @curso, notice: 'Curso was successfully updated.' }
+        format.html { redirect_to @curso, notice: 'Curso actualizado.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
--- app/models/curso.rb
+++ app/models/curso.rb
@@ -3,14 +3,17 @@ class Curso < ActiveRecord::Base
 	validates_presence_of :nombre, :message => "no ha sido ingresado."
 	validates_presence_of :formula, :message => "no ha sido ingresada."
 	
-	after_validation :validateFormula
+	before_save :validateFormula
 
-  def validateFormula
-
+  def validateFormula  
     acumulado = 0
     
     formulaPlus = self[:formula].split("+")
-   
+    if formulaPlus==''
+      errors.add(:formula," es incorrecta.")
+      return false
+    end
+
     for i in 0..formulaPlus.length-1
     	pesoXExam = formulaPlus[i].scan(/\d\d%[A-Z0-9]+/)
     	if pesoXExam.length == 0
